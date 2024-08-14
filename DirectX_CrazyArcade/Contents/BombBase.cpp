561de1e96c2164a174ca211bb27732bb1d2eed1d
김수원 <rla5233@naver.com>
2024-08-14 12:21:30 +0900

    김수원 : 변수명 수정

--- DirectX_CrazyArcade/Contents/BombBase.cpp
+++ DirectX_CrazyArcade/Contents/BombBase.cpp
@@ -82,7 +82,7 @@ void ABombBase::StateInit()
 
 			if (nullptr != SpawnPlayer)
 			{
-				SetBombPower(SpawnPlayer->GetBombPower());
+				BombPower = SpawnPlayer->GetBombPower() + 1;
 			}
 
 			CurPoint = AMapBase::ConvertLocationToPoint(GetActorLocation());
@@ -195,7 +195,7 @@ void ABombBase::SetCurPoint(FPoint _Point)
 void ABombBase::CreateLeftWave()
 {
 	// Left
-	for (int i = 1; i <= Power; i++)
+	for (int i = 1; i <= BombPower; i++)
 	{
 		FPoint WavePoint = { CurPoint.X - i, CurPoint.Y };
 		FVector WavePos = AMapBase::ConvertPointToLocation(WavePoint);
@@ -228,7 +228,7 @@ void ABombBase::CreateLeftWave()
 			}
 		}
 
-		if (i == Power)
+		if (i == BombPower)
 		{
 			AWave* LeftWave = GetWorld()->SpawnActor<AWave>("Wave").get();
 			LeftWave->SetActorLocation(WavePos);
@@ -245,7 +245,7 @@ void ABombBase::CreateLeftWave()
 void ABombBase::CreateRightWave()
 {
 	// Right
-	for (int i = 1; i <= Power; i++)
+	for (int i = 1; i <= BombPower; i++)
 	{
 		FPoint WavePoint = { CurPoint.X + i, CurPoint.Y };
 		FVector WavePos = AMapBase::ConvertPointToLocation(WavePoint);
@@ -278,7 +278,7 @@ void ABombBase::CreateRightWave()
 			}
 		}
 
-		if (i == Power)
+		if (i == BombPower)
 		{
 			AWave* LeftWave = GetWorld()->SpawnActor<AWave>("Wave").get();
 			LeftWave->SetActorLocation(WavePos);
@@ -295,7 +295,7 @@ void ABombBase::CreateRightWave()
 void ABombBase::CreateUpWave()
 {
 	// Up
-	for (int i = 1; i <= Power; i++)
+	for (int i = 1; i <= BombPower; i++)
 	{
 		FPoint WavePoint = { CurPoint.X, CurPoint.Y + i };
 		FVector WavePos = AMapBase::ConvertPointToLocation(WavePoint);
@@ -328,7 +328,7 @@ void ABombBase::CreateUpWave()
 			}
 		}
 
-		if (i == Power)
+		if (i == BombPower)
 		{
 			AWave* LeftWave = GetWorld()->SpawnActor<AWave>("Wave").get();
 			LeftWave->SetActorLocation(WavePos);
@@ -345,7 +345,7 @@ void ABombBase::CreateUpWave()
 void ABombBase::CreateDownWave()
 {
 	// Down
-	for (int i = 1; i <= Power; i++)
+	for (int i = 1; i <= BombPower; i++)
 	{
 		FPoint WavePoint = { CurPoint.X, CurPoint.Y - i };
 		FVector WavePos = AMapBase::ConvertPointToLocation(WavePoint);
@@ -378,7 +378,7 @@ void ABombBase::CreateDownWave()
 			}
 		}
 
-		if (i == Power)
+		if (i == BombPower)
 		{
 			AWave* LeftWave = GetWorld()->SpawnActor<AWave>("Wave").get();
 			LeftWave->SetActorLocation(WavePos);
--- DirectX_CrazyArcade/Contents/BombBase.h
+++ DirectX_CrazyArcade/Contents/BombBase.h
@@ -24,7 +24,7 @@ public:
 	void SetCurPoint(FPoint _Point);
 	inline void SetBombPower(int _Power)
 	{
-		Power = _Power + 1;
+		BombPower = _Power + 1;
 	}
 
 	inline void SetIdle()
@@ -64,7 +64,7 @@ private:
 	FPoint CurPoint = { -1, -1 };
 	const float ExplosionTime = 2.8f;
 	float ExplosionTimeCount = 0.0f;
-	int Power = -1;
+	int BombPower = -1;
 
 // FSM
 private:
